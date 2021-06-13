fa75c1eadeb4852a2a52649d889cd1b7c50fd398
Martin Persson <martin.persson@goli.nu>
2021-06-13 14:49:07 +0200

    Added update function

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,11 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import InputGroup from 'react-bootstrap/InputGroup'
 import Button from 'react-bootstrap/Button'
+import FormControl from 'react-bootstrap/FormControl'
 import { TodoContext } from '../contexts/todoContext'
 
 const TodoListItem = ({ todo }) => {
   const { dispatch } = useContext(TodoContext)
+  const [input, setInput] = useState(todo.description)
+  const [updateMode, setUpdateMode] = useState(false)
 
   const toggleTodo = (e) => {
     e.preventDefault()
@@ -22,12 +26,36 @@ const TodoListItem = ({ todo }) => {
       todo,
     })
   }
+
+  const updateTodo = (e) => {
+    e.preventDefault()
+    if (updateMode) {
+      dispatch({
+        type: 'UPDATE_TODO',
+        todo,
+        input,
+      })
+    }
+    setUpdateMode(!updateMode)
+  }
+
   return (
     <ListGroup.Item variant={todo.completed ? 'dark' : 'light'}>
-      <p style={{ textDecoration: todo.completed ? 'line-through' : null, color: 'black' }}>{todo.description}</p>
-      <ButtonGroup aria-label="Basic example">
-        <Button onClick={(e) => toggleTodo(e)} variant="info">Toggle completed</Button>
-        <Button onClick={(e) => deleteTodo(e)} variant="danger">Remove</Button>
+      {!updateMode ? (
+        <p style={{ textDecoration: todo.completed ? 'line-through' : null, color: 'black' }}>{todo.description}</p>
+      ) : (
+        <InputGroup size="sm" className="mb-3">
+          <FormControl
+            aria-label="Small"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </InputGroup>
+      )}
+      <ButtonGroup size="sm">
+        <Button disabled={updateMode} onClick={(e) => toggleTodo(e)} variant="success">Toggle completed</Button>
+        <Button onClick={(e) => updateTodo(e)} variant="info">{updateMode ? 'Save update' : 'toggle update'}</Button>
+        <Button disabled={updateMode} onClick={(e) => deleteTodo(e)} variant="danger">Remove</Button>
       </ButtonGroup>
     </ListGroup.Item>
   )
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -10,7 +10,7 @@ const removeTodo = (todo, action) => todo._id !== action.todo._id
 export const todoReducer = (state, action) => {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
-        return [...state, action.todo]
+      return [...state, action.todo]
     case ACTIONS.DELETE_TODO:
       return state.filter((todo) => removeTodo(todo, action))
     case ACTIONS.TOGGLE_TODO:
@@ -18,7 +18,13 @@ export const todoReducer = (state, action) => {
         ...action.todo,
         completed: !action.todo.completed
       }
-      return [...state.filter((todo) => removeTodo(todo, action)), newTodo ]
+      return [...state.filter((todo) => removeTodo(todo, action)), newTodo]
+    case ACTIONS.UPDATE_TODO:
+      const updatedTodo = {
+        ...action.todo,
+        description: action.input
+      }
+      return [...state.filter((todo) => removeTodo(todo, action)), updatedTodo]
     default:
       return state
   }
